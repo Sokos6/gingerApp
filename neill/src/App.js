0aef81f43b811a3bd55690fde430c223f14197b9
Will Sokolowski <will.sokolowski@gmail.com>
2018-12-04 16:43:46 -0700

    initial commit

---
+++ neill/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import CounterParent from './components/CounterParent';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <CounterParent />  
+      
+        
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ neill/src/components/Counter.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() {
+    var textStyle = {
+      fontSize: 72,
+      fontFamily: "sans-serif",
+      color: "#333",
+      fontWeight: "bold"
+  };
+
+  return (
+    <div style={textStyle}>
+      {this.props.display}
+    </div>
+  );
+}
+}
+
+export default Counter;
